e6cf241f2435b633d2cb48a0a66216ba87c8e4d2
Matthew Leeds <mwl458@gmail.com>
2016-11-19 23:33:23 -0600

    Print usage info for start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+display_usage() {
+    echo -e "\nUsage:\n$0 PATH_TO_EXECUTABLE\n"
+}
+
+if [[ $# -ne 1 ]]; then
+    display_usage
+    exit 1
+fi
+
+if [[ $@ == "--help" || $@ == "-h" ]]; then
+    display_usage
+    exit 0
+fi
+
 chmod +x buildimage.sh
 chmod +x scripts/*
 go build
